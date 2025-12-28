9d0e31135adfef63e755ea2dded2be9caad49c25
mytranng <mytran1109@gmail.com>
2025-12-28 13:04:10 +0700

    adjust var in prompt template

--- src/hipporag/prompts/templates/triple_extraction_vietnamese_law.py
+++ src/hipporag/prompts/templates/triple_extraction_vietnamese_law.py
@@ -19,11 +19,11 @@ ner_conditioned_re_frame = """Dựa vào Đoạn văn và Danh sách thực th�
 {passage}
 ```
 
-{named_entities}
+{named_entity_json}
 """
 
 # Ví dụ đầu vào (one-shot)
-ner_conditioned_re_input = ner_conditioned_re_frame.format(passage=one_shot_ner_paragraph, named_entities=one_shot_ner_output)
+ner_conditioned_re_input = ner_conditioned_re_frame.format(passage=one_shot_ner_paragraph, named_entity_json=one_shot_ner_output)
 
 # Ví dụ đầu ra tương ứng với ví dụ đầu vào
 ner_conditioned_re_output = """{"bộ ba": [
