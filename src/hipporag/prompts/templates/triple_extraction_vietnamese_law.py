43969c2d459b91aadb2fdcb3a24e9537fb403303
mytranng <mytran1109@gmail.com>
2025-12-28 08:51:12 +0700

    adjust variable in prompt template

--- src/hipporag/prompts/templates/triple_extraction_vietnamese_law.py
+++ src/hipporag/prompts/templates/triple_extraction_vietnamese_law.py
@@ -16,14 +16,14 @@ Hãy chú ý đến các yêu cầu sau:
 ner_conditioned_re_frame = """Dựa vào Đoạn văn và Danh sách thực thể có tên dưới đây, hãy trích xuất các mối quan hệ dưới dạng một danh sách các bộ ba.
 Đoạn văn:
 ```
-{đoạn văn}
+{doan_van}
 ```
 
-{json về bộ ba}
+{json_ve_bo_ba}
 """
 
 # Ví dụ đầu vào (one-shot)
-ner_conditioned_re_input = ner_conditioned_re_frame.format(passage=one_shot_ner_paragraph, named_entity_json=one_shot_ner_output)
+ner_conditioned_re_input = ner_conditioned_re_frame.format(doan_van=one_shot_ner_paragraph, json_ve_bo_ba=one_shot_ner_output)
 
 # Ví dụ đầu ra tương ứng với ví dụ đầu vào
 ner_conditioned_re_output = """{"bộ ba": [
