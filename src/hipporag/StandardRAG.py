7e25a9a78724db07292cea64db97dcd3be5638ba
mytranng <mytran1109@gmail.com>
2026-02-27 16:37:38 +0700

    comment out ipdb

--- src/hipporag/StandardRAG.py
+++ src/hipporag/StandardRAG.py
@@ -91,8 +91,8 @@ class StandardRAG:
                 embedding_model_name=self.global_config.embedding_model_name)(global_config=self.global_config,
                                                                               embedding_model_name=self.global_config.embedding_model_name)
 
-        import ipdb;
-        ipdb.set_trace()
+        # import ipdb;
+        # ipdb.set_trace()
 
         self.chunk_embedding_store = EmbeddingStore(self.embedding_model,
                                                     os.path.join(self.working_dir, "chunk_embeddings"),
